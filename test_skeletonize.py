1065877fd9b21f1446d948281fc0ddaacdc9d72e
yaaun <6322902+yaaun@users.noreply.github.com>
2023-06-14 00:59:50 +0200

    Testing of scipy.ndimage.label

--- test_skeletonize.py
+++ test_skeletonize.py
@@ -1,14 +1,20 @@
+import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
 
+import scipy.ndimage
 import skimage.io
 import skimage.morphology
 
 def main():
-    inimg = skimage.io.imread("BW_swirl.png")
-    skelImg = skimage.morphology.skeletonize(inimg)
+    #inimg = np.uint8(255 * skimage.io.imread("BW_swirl.png", as_gray=True))
+    inImg = skimage.io.imread("BW_swirl.png", as_gray=True)
+    skelImg = skimage.morphology.skeletonize(inImg)
     skimage.io.imshow(skelImg)
+    structElem = np.full((3,3), 1)
+    labelled, numObj = scipy.ndimage.label(skelImg, structElem)
     skimage.io.show()
+
 
 if __name__ == "__main__":
     main()
