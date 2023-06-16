42831b79c5b56c58d6003b569a59f8e5671bbacf
yaaun <6322902+yaaun@users.noreply.github.com>
2023-06-16 17:56:22 +0200

    Further improvements and refinements

--- biometr23/minutiae.py
+++ biometr23/minutiae.py
@@ -1,4 +1,7 @@
 import numpy as np
+import scipy.ndimage
+import skimage.morphology
+
 
 # Taken and adapted to NumPy from Przemysław Pastuszka crossing_number.py (https://github.com/przemekpastuszka/biometrics)
 def minutiae_at(pixels, y0, x0):
@@ -13,7 +16,7 @@ def minutiae_at(pixels, y0, x0):
             return 3
     return 0
 
-# Taken from Przemysław Pastuszka crossing_number.py (https://github.com/przemekpastuszka/biometrics)
+# Based on Przemysław Pastuszka crossing_number.py (https://github.com/przemekpastuszka/biometrics)
 # Adapted to numpy array image representation
 def minutiae_map(img):
     (max_y, max_x) = img.shape
@@ -27,3 +30,54 @@ def minutiae_map(img):
 
     return minutiae_locs
 
+
+def minutiae_map_filtered(img):
+    """
+    Map minutiae like :func:`minutiae_map` while filtering out
+    boundary effects and fragmented fingerprint ridges.
+
+    Parameters
+    ----------
+    img : ndarray((rows, columns))
+        Binary image of skeletonized fingerprint.
+
+    Returns
+    -------
+    minmap : ndarray((rows, columns)) of uint8
+        Matrix of the same size as the input image with minutiae locations
+        marked. 0 represents no feature, 1 represents a termination
+        and 3 represents a bifurcation.
+
+    """
+    full3x3 = np.ones((3,3))
+
+    # Settings for some size-specific morphology operations.
+    # The values given below are very roughly estimated and definitely
+    # not optimized.
+
+    # The erosion matrix will be of this size.
+    erosionDist = img.shape[1] // 40
+    # The minimum line size, in pixels, will be this many.
+    # Smaller (shorter) lines and objects will be ignored.
+    minLineSize = img.shape[1] // 20
+
+    # Assign numerical labels to distinct connected regions in the image.
+    labelled, numObjs = scipy.ndimage.label(img, full3x3)
+
+    # Remove small objects (short fragments of broken lines, in this case)
+    # connectivity = 2 == full3x3 footprint, in this case
+    noSmallObjsImg = skimage.morphology.remove_small_objects(labelled,
+                                 min_size=minLineSize, connectivity=2)
+
+    minmap = minutiae_map(noSmallObjsImg > 0) # run the minutiae mapping for surviving objects
+    termins = minmap == 1
+    bifurcs = minmap == 3
+
+    # Remove terminations detected at the edge of the fingerprint.
+    termins_hull = skimage.morphology.convex_hull_image(termins, True)
+    termins_hull_eroded = skimage.morphology.binary_erosion(termins_hull,
+                            footprint=np.full((erosionDist, erosionDist), True))
+    termins_hull_bound = termins_hull ^ termins_hull_eroded
+    minmap[termins_hull_bound] = 0 # or termins[termins_hull_bound] = False if termins is used further
+
+    return minmap
--- test_minutiae.py
+++ test_minutiae.py
@@ -1,5 +1,5 @@
 import numpy as np
-from biometr23.minutiae import minutiae_map
+from biometr23.minutiae import minutiae_map_filtered
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -13,19 +13,9 @@ if __name__ == "__main__":
         "skel_1_8.png",
         as_gray=True)
     binImg = img > 0
-    minmap = minutiae_map(binImg)
+    minmap = minutiae_map_filtered(binImg)
     termins = minmap == 1
     bifurcs = minmap == 3
-
-    # Remove spurious terminations at the bounds of the fingerprint.
-    termins_hull = skimage.morphology.convex_hull_image(termins, True)
-    termins_hull_eroded = skimage.morphology.binary_erosion(termins_hull,
-                                                            footprint=np.full((11,11), True))
-    termins_hull_bound = termins_hull ^ termins_hull_eroded
-    plt.imshow(termins_hull_bound, cmap="binary")
-    plt.colorbar()
-    plt.show()
-    termins[termins_hull_bound] = False
 
     #red_templ = np.full((minmap.shape[0], minmap.shape[1], 3), [255, 0, 0])
     #grn_templ = np.full((minmap.shape[0], minmap.shape[1], 3), [0, 255, 0])
@@ -37,5 +27,5 @@ if __name__ == "__main__":
 
     #skimage.io.imshow(minmap)
     #skimage.io.show()
-    skimage.io.imsave("a.png", outImg)
-    skimage.io.show()
+    skimage.io.imsave("_out.png", outImg)
+    #skimage.io.show()
